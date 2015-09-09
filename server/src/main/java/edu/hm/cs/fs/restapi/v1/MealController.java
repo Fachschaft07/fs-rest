fd7c2835396be1ecd10d7bf58b36696238dffd96
Fabio Hellmann <fhellmann93@googlemail.com>
2015-09-09 18:40:20 +0200

    BugFix
    The filter wasn't correct, so the meals for today and the future were
    not shown.

--- server/src/main/java/edu/hm/cs/fs/restapi/v1/MealController.java
+++ server/src/main/java/edu/hm/cs/fs/restapi/v1/MealController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,14 +27,23 @@ public class MealController {
      * @param location
      * @return
      * @throws IOException 
-     * @throws MalformedURLException 
-     * @throws XPathExpressionException 
+     * @throws XPathExpressionException
      */
     @RequestMapping("/rest/api/1/meal")
-    public List<Meal> getMeals(@RequestParam(value = "location") StudentWorkMunich location) throws MalformedURLException, XPathExpressionException, IOException {
+    public List<Meal> getMeals(@RequestParam(value = "location") StudentWorkMunich location) throws XPathExpressionException, IOException {
         return new MealParser(location).parse()
                 .stream()
-                .filter(meal -> new Date().after(meal.getDate()))
+                .filter(meal -> {
+                    Calendar today = Calendar.getInstance();
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(meal.getDate());
+
+                    // Is the meal today or in the future?
+                    return cal.after(today)
+                            || today.get(Calendar.YEAR) == cal.get(Calendar.YEAR)
+                            && today.get(Calendar.MONTH) == cal.get(Calendar.MONTH)
+                            && today.get(Calendar.DATE) == cal.get(Calendar.DATE);
+                })
                 .collect(Collectors.toList());
     }
 }
